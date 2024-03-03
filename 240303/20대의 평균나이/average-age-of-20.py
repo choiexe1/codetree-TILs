e18be88e941d58d943380517f3a2e824375d4956
choiexe1 <161970433+choiexe1@users.noreply.github.com>
2024-03-03 18:37:30 +0900

    Title: 20대의 평균나이, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240303/20대의 평균나이/average-age-of-20.py
+++ 240303/20대의 평균나이/average-age-of-20.py
@@ -2,9 +2,9 @@ cnt = 0
 sum = 0
 while True:
     age = int(input())
-    if age < 30 or age > 19:
-        cnt += 1
-        sum += age
-    else:
+    if age > 29 or age < 20:
         print(f'{sum / cnt:.2f}')
         break
+    else:
+        cnt += 1
+        sum += age
