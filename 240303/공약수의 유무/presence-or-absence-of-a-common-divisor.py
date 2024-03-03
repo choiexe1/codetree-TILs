487f84d9153e163ddf1acb3cd887a69e601cc5b4
choiexe1 <161970433+choiexe1@users.noreply.github.com>
2024-03-03 19:47:02 +0900

    Title: 공약수의 유무, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240303/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240303/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -1,10 +1,8 @@
 a, b = map(int, input().split())
 satisfied = False
 
-for i in range(a, b):
-    if 1920 % i == 0:
-        satisfied = True
-    if 2880 % i == 0:
+for i in range(a, b + 1):
+    if 1920 % i == 0 and 2880 % i == 0:
         satisfied = True
 
 if satisfied:
