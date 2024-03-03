99f59a9a9a1a7c896c3a6852310a3024676fba0d
choiexe1 <161970433+choiexe1@users.noreply.github.com>
2024-03-03 16:20:39 +0900

    Title: 두 사람, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240303/두 사람/two-person.py
+++ 240303/두 사람/two-person.py
@@ -1,8 +1,10 @@
 a_age, a_gen = input().split()
 b_age, b_gen = input().split()
 
-if int(a_age) >= 19 and int(b_age) >= 19:
+if int(a_age) >= 19 or int(b_age) >= 19:
     if a_gen == "M" or b_gen == "M":
         print(1)
+    else:
+        print(0)
 else:
     print(0)
