662a2e4306721738673c16fde921c9807780d310
choiexe1 <161970433+choiexe1@users.noreply.github.com>
2024-03-03 18:35:26 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240303/20대의 평균나이/average-age-of-20.py
+++ 240303/20대의 평균나이/average-age-of-20.py
@@ -2,9 +2,9 @@ cnt = 0
 sum = 0
 while True:
     age = int(input())
-    if age > 29:
-        print(f'{sum / cnt:.2f}')
-        break
-    else:
+    if age < 30:
         cnt += 1
         sum += age
+    else:
+        print(f'{sum / cnt:.2f}')
+        break
