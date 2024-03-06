7ad61f680c1396a383ae077c3c19262e522c3a9b
choiexe1 <161970433+choiexe1@users.noreply.github.com>
2024-03-07 00:10:44 +0900

    Title: 별 그리기, Time: 157ms, Memory: 27MB, Status: Passed - CodeTree

--- 240307/별 그리기/star-drawing.py
+++ 240307/별 그리기/star-drawing.py
@@ -6,9 +6,10 @@ for i in range(n):
     for k in range(2 * i + 1):
         print("*", end="")
     print()
-for i in range(n):
-    for j in range(i + 1):
+    
+for i in range(n - 1, 0, -1):
+    for _ in range(n - i):
         print(" ", end="")
-    for k in range(n - (i * 2)):
+    for _ in range(2 * i - 1):
         print("*", end="")
     print()
