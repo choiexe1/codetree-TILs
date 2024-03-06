ad6b7a61394166f0ee31f23320e71be34b621d40
choiexe1 <161970433+choiexe1@users.noreply.github.com>
2024-03-07 00:08:55 +0900

    Title: 별 그리기, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240307/별 그리기/star-drawing.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(n - i - 1):
+        print(" ", end="")
+    for k in range(2 * i + 1):
+        print("*", end="")
+    print()
+for i in range(n):
+    for j in range(i + 1):
+        print(" ", end="")
+    for k in range(n - (i * 2)):
+        print("*", end="")
+    print()
