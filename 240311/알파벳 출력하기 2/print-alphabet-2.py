4abbc672962e782e7530b592b7410902ad5f3c2e
choiexe1 <161970433+choiexe1@users.noreply.github.com>
2024-03-11 15:20:48 +0900

    Title: 알파벳 출력하기 2, Time: 126ms, Memory: 73MB, Status: Passed - CodeTree

--- 240311/알파벳 출력하기 2/print-alphabet-2.py
+++ 240311/알파벳 출력하기 2/print-alphabet-2.py
@@ -5,6 +5,8 @@ for i in range(n, 0, -1):
     for j in range(i, n):
         print(" ", end=" ")
     for k in range(i):
+        if chr(c) == '[':
+            c = 65
         print(chr(c), end=" ")
         c += 1
     print()
