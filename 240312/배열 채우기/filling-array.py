b9daeeaf2d77603d9146ab3489b299f24bb6026d
choiexe1 <161970433+choiexe1@users.noreply.github.com>
2024-03-12 15:34:40 +0900

    Title: 배열 채우기, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 240312/배열 채우기/filling-array.py
+++ 240312/배열 채우기/filling-array.py
@@ -5,6 +5,8 @@ for i in range(len(arr)):
     if arr[i] != 0:
         x.append(arr[i])
     else:
-        for j in range(len(x)):
-            print(x.pop(), end=" ")
         break
+
+
+for j in range(len(x)):
+    print(x.pop(), end=" ")
