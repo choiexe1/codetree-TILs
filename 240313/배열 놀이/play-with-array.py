ce0a8fa2b6746d90e1667b64145c696b5aa4750a
choiexe1 <161970433+choiexe1@users.noreply.github.com>
2024-03-13 20:38:26 +0900

    Title: 배열 놀이, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240313/배열 놀이/play-with-array.py
+++ 240313/배열 놀이/play-with-array.py
@@ -1,25 +1,27 @@
 n, q = map(int, input().split())
 e = list(map(int, input().split()))
 
+def one(index):
+    print(e[index - 1])
+
+def two(integer):
+    is_exist = e.count(integer)
+
+    if is_exist:
+        print(e.index(integer) + 1)
+    else:
+        print(0)
+
+def three(a, b):
+    for i in range(a - 1, b):
+        print(e[i], end=" ")
+
 for _ in range(q):
     query = tuple(map(int, input().split()))
 
     if query[0] == 1:
-        print(e[query[1] - 1])
+        one(query[1])
     elif query[0] == 2:
-        idx = [0] * max(e)
-        min_idx = -1
-
-        for index, integer in enumerate(e):
-            if integer == query[1]:
-                if min_idx == -1:
-                    min_idx = index
-                idx[integer] += 1
-        
-        if min_idx != -1:
-            print(min_idx + 1)
-        else:
-            print(0)
-    elif query[0] == 3:
-        for i in range(query[1] - 1, query[2]):
-            print(e[i], end=" ")
+        two(query[1])
+    else:
+        three(query[1], query[2])
