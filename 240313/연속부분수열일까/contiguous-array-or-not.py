c61a0e6886f832827b46db853609a3ed1329bfea
choiexe1 <161970433+choiexe1@users.noreply.github.com>
2024-03-13 22:20:14 +0900

    Title: 연속부분수열일까, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240313/연속부분수열일까/contiguous-array-or-not.py
+++ 240313/연속부분수열일까/contiguous-array-or-not.py
@@ -2,19 +2,26 @@ n1, n2 = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
-x = a.index(b[0])
-is_sequnce = True
+def find_index(array, integer):
+    for i, e in enumerate(array):
+        if integer == e:
+            return i
+    return -1
+
+index = find_index(a, b[0])
 
 i = 0
-while i <= len(b):
-    if i == len(b):
-        break
-    if a[x] != b[i]:
-        is_sequnce = False
-        break
-    else:
-        x += 1
-        i += 1
+
+is_sequnce = False
+
+if index:
+    while i <= len(b) - 1:
+        if a[index] == b[i]:
+            index += 1
+            i += 1
+        else:
+            break
+    is_sequnce = True
 
 if is_sequnce:
     print("Yes")
