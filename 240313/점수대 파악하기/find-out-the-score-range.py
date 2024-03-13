02bd9674c741ef194cbddb470d503f076c4556cc
choiexe1 <161970433+choiexe1@users.noreply.github.com>
2024-03-13 18:10:10 +0900

    Title: 점수대 파악하기, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240313/점수대 파악하기/find-out-the-score-range.py
@@ -0,0 +1,13 @@
+n = list(map(int, input().split()))
+x = [0 for _ in range(10)]
+
+for e in n:
+    if e == 0:
+        break
+    if e // 10 > 0:
+        x[e // 10 - 1] += 1
+
+s = 100
+for i in range(len(x) - 1, -1, -1):
+    print(f"{s} - {x[i]}")
+    s -= 10
