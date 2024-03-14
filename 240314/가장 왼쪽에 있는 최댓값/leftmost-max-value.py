1df49836e8ca90c2e839169f02140f20f482852e
choiexe1 <161970433+choiexe1@users.noreply.github.com>
2024-03-14 19:59:38 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 5018ms, Memory: 238MB, Status: Time Limit Exceed - CodeTree

---
+++ 240314/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -0,0 +1,22 @@
+n = int(input())
+m = list(map(int, input().split()))
+
+max = m[0]
+index = -1
+
+for i, e in enumerate(m):
+    if e > max:
+        max = e
+        index = i
+
+print(index + 1, end=" ")
+max = 0
+
+while True:
+    if index == 0:
+        break
+    for i in range(index):
+        if m[i] > max:
+            max = m[i]
+            index = i
+    print(index + 1, end=" ")
