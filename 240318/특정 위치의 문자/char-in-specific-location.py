3309448c3f0fb1b6ff6273bffc368e7472708bdb
choiexe1 <161970433+choiexe1@users.noreply.github.com>
2024-03-18 13:30:57 +0900

    Title: 특정 위치의 문자, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240318/특정 위치의 문자/char-in-specific-location.py
+++ 240318/특정 위치의 문자/char-in-specific-location.py
@@ -1,8 +1,14 @@
 x = input()
 n = ['L', 'E', 'B', 'R', 'O', 'S']
+t = False
 
 for i, e in enumerate(n):
     if x == e:
-        print(i)
-    else:
-        print("None")
+        t = True
+        x = i
+
+
+if t:
+    print(x)
+else:
+    print("None")
